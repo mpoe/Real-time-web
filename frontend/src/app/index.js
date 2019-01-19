2f37873393b0713df64c0da3c7b323366ec27ea7
Morten Osterlund <mpoe@airofit.com>
2019-01-19 23:30:54 +0100

    Created app container (preparation for redux)

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,6 +1,6 @@
-import {App} from './containers/App';
+import {AppContainer} from './containers/App';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<AppContainer />, document.getElementById('app'))
